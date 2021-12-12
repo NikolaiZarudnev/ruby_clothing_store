090b998a86ee7fc700ab529b34fa20614d31a488
Николай Заруднев <nikolaizarudnrv@gmail.com>
2021-12-13 00:58:12 +0700

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,3 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
 end
-# name:"admin" password:"$2a$12$UQWzsvqvhK0sL5MJYPGXw.zxLW19/qd8ekGWUE638NX1IiEKRBi.G"
