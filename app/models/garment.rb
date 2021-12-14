a1fba6d4a573a408cb821a3bfa7b38b2b61b851d
Николай Заруднев <nikolaizarudnrv@gmail.com>
2021-12-14 11:58:48 +0700

    some lil changes

--- app/models/garment.rb
+++ app/models/garment.rb
@@ -2,5 +2,6 @@ class Garment < ApplicationRecord
   belongs_to :category
   validates :name, presence: true
   validates :description, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: {only_float: true, greater_than: 0}
+  
 end
