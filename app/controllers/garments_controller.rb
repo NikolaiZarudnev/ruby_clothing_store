3afa4ff565b5a8164a58caa9767c3523280ec66d
Shevja <vlsdki@gmail.com>
2021-12-17 21:01:15 +0700

    displays garments img, and create folder for garments img in assets

--- app/controllers/garments_controller.rb
+++ app/controllers/garments_controller.rb
@@ -14,6 +14,13 @@ class GarmentsController < ApplicationController
   end
   def create
     @category = Category.find(params[:category_id])
+    p '-----------------------------------------------------------------------'
+    uploaded_io = garment_params["image"]
+    File.open(Rails.root.join('app', 'assets', 'images', 'garment_img', uploaded_io.original_filename), 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+    params['garment']['image'] = uploaded_io.original_filename
+    p '-----------------------------------------------------------------------'
     @garment = @category.garments.create(garment_params)
     redirect_to category_path(@category)
   end
