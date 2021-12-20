34e6f4ae94d23b0cda5a712fef2f71fb7089e4fa
Николай Заруднев <nikolaizarudnrv@gmail.com>
2021-12-20 20:38:59 +0700

    image_load

--- app/controllers/garments_controller.rb
+++ app/controllers/garments_controller.rb
@@ -8,19 +8,17 @@ class GarmentsController < ApplicationController
     @category = Category.find(params[:category_id])
     @garment = Garment.new
   end
+
   def edit
     @category = Category.find(params[:category_id])
     @garment = @category.garments.find(params[:id])
   end
+
   def create
     @category = Category.find(params[:category_id])
-    if params['garment']['image'] == nil then params['garment']['image'] = 'none.jpg'
+    if params['garment']['image'].nil? then params['garment']['image'] = 'none.jpg'
     else
-      uploaded_io = garment_params["image"]
-      File.open(Rails.root.join('app', 'assets', 'images', 'garment_img', uploaded_io.original_filename), 'wb') do |file|
-        file.write(uploaded_io.read)
-      end
-      params['garment']['image'] = uploaded_io.original_filename
+      image_load
     end
     @garment = @category.garments.create(garment_params)
     redirect_to category_path(@category)
@@ -29,22 +27,18 @@ class GarmentsController < ApplicationController
   def update
     @category = Category.find(params[:category_id])
     @garment = @category.garments.find(params[:id])
-    
-    if params['garment']['image'] != nil
-      uploaded_io = garment_params["image"]
-      File.open(Rails.root.join('app', 'assets', 'images', 'garment_img', uploaded_io.original_filename), 'wb') do |file|
-        file.write(uploaded_io.read)
-      end
-      params['garment']['image'] = uploaded_io.original_filename
+
+    unless params['garment']['image'].nil?
+      image_load
     end
 
     @garment.update(garment_params)
 
     redirect_to category_path(@category)
     #  redirect_to @garment
-    #else
-     # render 'edit'
-    #end
+    # else
+    # render 'edit'
+    # end
   end
 
   def destroy
@@ -58,5 +52,14 @@ class GarmentsController < ApplicationController
 
   def garment_params
     params.require(:garment).permit(:name, :description, :price, :color, :image)
+  end
+
+  def image_load
+    uploaded_io = garment_params['image']
+    File.open(Rails.root.join('app', 'assets', 'images', 'garment_img', uploaded_io.original_filename),
+    'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+    params['garment']['image'] = uploaded_io.original_filename
   end
 end
