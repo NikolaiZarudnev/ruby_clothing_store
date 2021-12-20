eb39847b6cf6c9c74abd5f188799bc57c5a7beee
Николай Заруднев <nikolaizarudnrv@gmail.com>
2021-12-20 21:57:39 +0700

    del comm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,6 @@ class ApplicationController < ActionController::Base
 
   def log_out
     session.delete :user_id
-    # @current_user = nil
   end
   helper_method :current_user, :user_signed_in?, :log_in
 end
