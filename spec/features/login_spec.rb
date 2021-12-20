19b29c17879118bfbd59fff65b405dcdae48fa3a
NikolaiZarudnev <nikolaizarudnrv@gmail.com>
2021-12-20 21:56:22 +0700

    spec features

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-describe 'the signin process', type: :feature do
+describe 'the log in process', type: :feature do
  before :each do
    User.create(email: 'user1@gmail.com', password: 'password', name: 'User1')
  end
- it 'signs @user in' do
-   visit '/users/sign_in'
+ it 'log @user in' do
+   visit '/session/new'
    fill_in 'Email', with: 'user1@gmail.com'
    fill_in 'Password', with: 'password'
-   click_button 'Log in'
+   click_button 'Save'
   expect(current_path).to eq(root_path)
   expect(page).to have_text('WELCOME, User1')
  end
---
+++ spec/features/search_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'the search process', type: :feature do
+ before :each do
+   Garment.create(name: 'Abibas')
+ end
+ it 'search Abibas' do
+   visit '/categories'
+   fill_in 'Поиск', with: 'Abibas'
+   click_button 'Поиск'
+  expect(current_path).to eq("/search")
+  expect(page).to have_text('Результаты поиска')
+ end
+end
