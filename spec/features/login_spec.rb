e233ca93c8c950d9531213296b136518a4d78db3
NikolaiZarudnev <nikolaizarudnrv@gmail.com>
2021-12-20 23:28:31 +0700

    rubocop

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 describe 'the log in process', type: :feature do
- before :each do
-   User.create(email: 'user1@gmail.com', password: 'password', name: 'User1')
- end
- it 'log @user in' do
-   visit '/session/new'
-   fill_in 'Email', with: 'user1@gmail.com'
-   fill_in 'Password', with: 'password'
-   click_button 'Save'
-  expect(current_path).to eq(root_path)
-  expect(page).to have_text('WELCOME, User1')
- end
+  before :each do
+    User.create(email: 'user1@gmail.com', password: 'password', name: 'User1')
+  end
+  it 'log @user in' do
+    visit '/session/new'
+    fill_in 'Email', with: 'user1@gmail.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Save'
+    expect(current_path).to eq(root_path)
+    expect(page).to have_text('WELCOME, User1')
+  end
 end
--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 describe 'the sign up process', type: :feature do
+  it 'sign @user up' do
+    visit '/users/new'
 
-
- it 'sign @user up' do
-   visit '/users/new'
-
-   fill_in 'Email', with: 'user1@gmail.com'
-   fill_in 'Name', with: 'user1'
-   fill_in 'Password', with: 'password'
-   fill_in 'Password confirmation', with: 'password'
-   click_button 'Create User'
-  expect(current_path).to eq("/")
-  expect(page).to have_text('WELCOME, user1')
- end
+    fill_in 'Email', with: 'user1@gmail.com'
+    fill_in 'Name', with: 'user1'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Create User'
+    expect(current_path).to eq('/')
+    expect(page).to have_text('WELCOME, user1')
+  end
 end
